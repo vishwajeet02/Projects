51c44ef7bd629c52701da0d02f56c5b3bc2256eb
Vishwajeet Mehta <vishwajeet25200012@gmail.com>
2025-02-12 21:05:07 +0530

    Updation-3

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import list_ds
-
+import tuple_ds
 my_list = list_ds.create_list()
 list_ds.add_element(my_list,5)
 
 print(my_list)
+
+my_tuple = tuple_ds.create_tuple([1,2,3,4])
+print(tuple_ds.length(my_tuple))
---
+++ tuple_ds.py
@@ -0,0 +1,5 @@
+def create_tuple(my_list):
+    return tuple(my_list)
+
+def length(my_tuple):
+    return len(my_tuple)
