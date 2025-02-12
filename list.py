a627bac1fd10d285eef2dd4cc9a8c68a8d24869f
Vishwajeet Mehta <vishwajeet25200012@gmail.com>
2025-02-12 20:55:54 +0530

    Updation-2

--- list.py
+++
@@ -1,7 +0,0 @@
-def create_list():
-    return []
-
-
-def add_element(my_list,element):
-    my_list.append(element)
-    return my_list
---
+++ main.py
@@ -0,0 +1,6 @@
+import list_ds
+
+my_list = list_ds.create_list()
+list_ds.add_element(my_list,5)
+
+print(my_list)
